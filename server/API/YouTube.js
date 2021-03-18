0c12d533d6a0d4d3eca6cec5b65a1cca2565a4b4
denys_glu <denis.glukhovskyy@gmail.com>
2021-03-18 16:08:22 -0700

    added pagination, page numbers are not working yet

--- server/API/YouTube.js
+++ server/API/YouTube.js
@@ -1,29 +1,28 @@
 const axios = require("axios");
 module.exports.YouTube = async(req, channelId = "UClqhvGmHcvWL9w3R48t9QXQ", searchTerm = "") => {
+  console.log(`req.params.searchTerm`, req.params.searchTerm)
   try {
     const response = await axios
       .get(`https://www.googleapis.com/youtube/v3/search`, {
         params: {
-          key: 'AIzaSyAJZRMJD08mXDZKMkAFolSOjpM1gk4I9c4',
+          key: 'AIzaSyCvgaZ8VF3Q_H_mDq7XJCzUeeF5jfyUsng',
           maxResults: req.params.amount || 12,
           type: 'video',
           channelId: req.params.channelId || channelId,
           q: req.params.searchTerm || searchTerm,
           part: 'snippet',
-          order: 'date'
+          order: 'date',
+          pageToken: req.params.pageToken || ""
         }
       })
-      // console.log(`response,data`, response.data)
-
-    return { status: 'Success', data: response.data.items }
-    // if (response.data.items.length > 0) {
-    //   res.json({ status: 'Success', data: response.data.items })
-    // } else {
-    //   res.json({ status: 'Error', data: [], message: 'No videos were found' })
-    // }
-
+    console.log(`response.data`, response.data)
+    return ({
+      status: 'Success',
+      videos: response.data.items,
+      prevPageToken: response.data.prevPageToken,
+      nextPageToken: response.data.nextPageToken
+    })
   } catch (err) {
     return { status: 'Error', message: err, data: [] }
-    // res.json({ status: 'Error', message: err })
   }
 }
--- server/routes/myRoutes.routes.js
+++ server/routes/myRoutes.routes.js
@@ -1,10 +1,9 @@
 const YoutubeAPIController = require("../controllers/youtubeAPI.controller");
 
 module.exports = app => {
-  app.get("/api/videos", YoutubeAPIController.getAllVideos);
-
-  app.get("/api/videos/:amount/:pages", YoutubeAPIController.getSomeVideos);
 
   app.get("/api/videos/:searchTerm", YoutubeAPIController.getSpecificVideos);
+
+  app.get("/api/videos/:searchTerm/:pageToken", YoutubeAPIController.getSomeVideos);
 
 };
