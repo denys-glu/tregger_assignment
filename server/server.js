f5db52a758f1381d873032b991a98e2c1f49bd6c
denys_glu <denis.glukhovskyy@gmail.com>
2021-03-22 12:04:19 -0700

    added static file support...

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -7,5 +8,10 @@ const app = express();
 app.use(cors(), express.urlencoded({ extended: true }), express.json());
 
 require("./routes/myRoutes.routes")(app);
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+});
 
 const server = app.listen(PORT, () => console.log(`The server is all fired up on port ${PORT}`));
